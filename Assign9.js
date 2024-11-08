f74aca3db2d2a2aba98341e7a83533e444f634b9
pratham-mistari <prathamesh.mistari05@gmail.com>
2024-11-08 16:26:47 +0530

    Added Assignment 9

---
+++ Assign9.js
@@ -0,0 +1,10 @@
+    function action(){
+        if(document.querySelector("#chk1").checked)
+        {
+            document.write("Thank You For Accepting License Agriment");
+        }
+        else
+        {
+            document.write("Please Accept The License Agriment");
+        }
+    }
