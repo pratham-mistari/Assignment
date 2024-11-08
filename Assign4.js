c6b0710b655553ae1a798a9c25e5ee1b413c6a00
pratham-mistari <prathamesh.mistari05@gmail.com>
2024-11-08 10:18:53 +0530

    Added Javascript in Assignment 4

---
+++ Assign4.js
@@ -0,0 +1,13 @@
+let num = prompt("Enter The Number : ");
+num = eval(num);
+if(num > 0){
+    document.write("Entered Number Is Positive ");
+}
+
+else if(num<0){
+    document.write("Entered Number Is Negative");
+}
+
+else{
+    document.write("Entered Number Is Zero");
+}
